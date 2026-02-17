70d7a34e5327fbd6c88e13e8d0e1d923db206e66
Joao Victor S Tolentino <joao.tolentino@discente.ufg.br>
2026-02-17 12:32:49 -0300

    A simple text editor with a UI and font size and type selection.

---
+++ App.java
@@ -0,0 +1,7 @@
+// Main class
+public class App {
+    public static void main(String[] args){
+        // Instantiate the Text Editor with a window.
+        new TextEditor();
+    }
+}
---
+++ launcher.bat
@@ -0,0 +1,5 @@
+:: Compile the files using javac
+javac *.java
+
+:: Run the main class in App
+java App & cls
